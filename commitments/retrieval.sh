ded4380bc94af10248a1a326da39397f58eea3fe
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-21 20:11:50 +0800

    Add commitments to the kepler package

---
+++ commitments/retrieval.sh
@@ -0,0 +1,14 @@
+# First, we check status for the sake of checking
+git status
+
+# Next, we must add all the files cuz why not right
+git add */*retrieve*.* */*Retrieve*.*
+
+# Commit files with a very specific command
+git commit -m "Update Retrieval Process"
+
+# Push to origin repository
+git push origin
+
+# Recheck status (for debugging purposes)
+git status
