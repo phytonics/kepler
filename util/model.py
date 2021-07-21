5647f4e39de7df0cbd1717766b2f49ef53813835
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-21 23:04:34 +0800

    Add model

---
+++ util/model.py
@@ -0,0 +1,28 @@
+import numpy as np
+from dataclasses import dataclass
+
+def robust_mean(y, cut):
+    absdev = np.abs(y - np.median(y))
+    sigma = 1.4826 * np.median(absdev)
+    sigma = np.std(y[absdev <= cut * (1.253 * np.mean(absdev) if np.log1    0(sigma) < 24 else sigma)])
+    sc = cut if cut > 1 else 1
+    if cut <= 4.5:
+        sigma /= (-0.15405 + 0.90723 * sc - 0.23584 * sc ** 2 + 0.020142     * sc ** 3)
+
+    mask = absdev <= cut * sigma
+    mean, sigma = np.mean(y[mask]), np.std(y[mask])
+    if cut <= 4.5:
+        sigma /= (-0.15405 + 0.90723 * sc - 0.23584 * sc ** 2 + 0.020142     * sc ** 3)
+    mean_stddev = sigma / np.sqrt(len(y) - 1)
+    return mean, mean_stddev, mask
+
+
+@dataclass
+class PeriodicEvent:
+    period: float
+    duration: float
+    t0: float
+
+    def equals(self, other: PeriodicEvent, period_rtol=0.001, t0_durations=1):
+        return np.arcsin(np.sin((np.pi * (self.t0 - other.t0) % other.period
+) / other.period)) * (other.period / np.pi) < t0_durations * other_event.duration if not np.isclose(self.period, other.period, rtol=period_rtol, atol=1e-8) else False
