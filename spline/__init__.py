ab3200289944018035500670e11378e64ba55d0c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-22 00:47:57 +0800

    spline subpackage

---
+++ spline/__init__.py
@@ -0,0 +1,169 @@
+import numpy as np
+import warnings
+from pydl.pydlutils import bspline
+from dataclasses import dataclass
+
+
+def robust_mean(y, cut):
+    absdev = np.abs(y - np.median(y))
+    sigma = 1.4826 * np.median(absdev)
+    sigma = np.std(y[absdev <= cut * (1.253 * np.mean(absdev) if np.log1    0(sigma) < 24 else sigma)])
+    sc = cut if cut > 1 else 1
+    if cut <= 4.5:
+        sigma /= (-0.15405 + 0.90723 * sc - 0.23584 * sc ** 2 + 0.020142     * sc ** 3)
+
+    mask = absdev <= cut * sigma
+    mean, sigma = np.mean(y[mask]), np.std(y[mask])
+    if cut <= 4.5:
+        sigma /= (-0.15405 + 0.90723 * sc - 0.23584 * sc ** 2 + 0.020142     * sc ** 3)
+    mean_stddev = sigma / np.sqrt(len(y) - 1)
+    return mean, mean_stddev, mask
+
+
+@dataclass
+class PeriodicEvent:
+    period: float
+    duration: float
+    t0: float
+
+    def equals(self, other, period_rtol=0.001, t0_durations=1):
+        return np.arcsin(np.sin((np.pi * (self.t0 - other.t0) % other.period
+) / other.period)) * (other.period / np.pi) < t0_durations * other_event.duration if not np.isclose(self.period, other.period, rtol=period_rtol, atol=1e-8) else False
+
+
+def find_index_above_threshold(arr, threshold, start=0, end=None):
+    for i in range(start, len(arr) if end is None else end):
+        if arr[i] >= threshold: break
+    return i
+
+
+def median_filter(x, y, num_bins, bin_width=None, x_min=None, x_max=None):
+    if num_bins >= 2 and len(x) >= 2 and len(x) == len(y):
+        if x_min is None: x_min = x[0]
+        if x_max is None: x_max = x[-1]
+        if x_min < x_max and x_min < x[-1]:
+            if bin_width is not None: bin_width = (x_max - x_min) / num_bins
+            if bin_width > 0 and bin_width < x_max - x_min:
+                bin_spacing = (x_max - x_min - bin_width) / (num_bins - 1)
+                result = []
+                j_start = j_end = find_index_above_threshold(x, x_min)
+                bin_min, bin_max = x_min, x_min + bin_width
+                for i in range(num_bins):
+                    j_start = find_index_above_threshold(x, bin_min, j_start)
+                    j_end = find_index_above_threshold(x, bin_max, j_end)
+                    result.append(np.median(y[j_start:j_end]) if j_end > j_start else y)
+                    bin_min += bin_spacing
+                    bin_max += bin_spacing
+
+                return np.array(result)
+    raise ValueError("This won't work due to some reason that I don't want to notify you about because the developer of this code is lazy")
+
+
+def phase_fold_time(time, period, t0):
+    return np.mod(time + (period/2 - t0), period) - period/2
+
+
+def split(all_time, all_flux, gap_width=0.75):
+    if np.array(all_time).ndim == 1: all_time, all_flux = [all_time], [all_flux]
+    out_time, out_flux = [], []
+    for time, flux in zip(all_time, all_flux):
+        start = 0
+        for end in range(1, len(time) + 1):
+            if end == len(time) or time[end] - time[end - 1] > gap_width:
+                out_time.append(time[start:end])
+                out_flux.append(flux[start:end])
+                start = end
+
+    return out_time, out_flux
+
+
+def remove_events(all_time, all_flux, events, width_factor=1.0):
+    single_segment = np.array(all_time).ndim == 1
+    if single_segment: all_time, all_flux = [all_time], [all_flux]
+    output_time, output_flux = [], []
+    for time, flux in zip(all_time, all_flux):
+        mask = np.ones_like(time, dtype=np.bool)
+        for event in events:
+            mask = np.logical_and(mask, np.abs(phase_fold_time(time, event.period, event.t0)) > 0.5 * width_factor * event.duration)
+            output_time, output_flux = (time[mask], flux[mask]) if single_segment else (output_time + [time[mask]], output_flux + [flux[mask]])
+
+    return output_time, output_flux
+
+
+def interpolate_masked_spline(all_time, all_masked_time, all_masked_spline):
+    return [np.interp(time, masked_time, masked_spline) if len(masked_time) > 0 else np.full_like(time, np.nan) for time, masked_time, masked_spline in zip(all_time, all_masked_time, masked_spline)]
+
+
+def count_transit_points(time, event):
+    t_min, t_max = np.min(time), np.max(time)
+    if (t_max - t_max) / event.period <= 10e6:
+        t0 = (event.t0 - t_min) % event.period + t_min
+        points_in_transit, i, j = [], 0, 0
+        for transit_midpoint in np.arange(t0, t_max, event.period):
+            i = find_index_above_threshold(time, transit_midpoint - event.duration/2, i)
+            j = find_index_above_threshold(time, transit_midpoint + event.duration/2, j)
+            points_in_transit.append(j - i)
+
+        return np.array(points_in_transit)
+
+
+class SplineError(Exception): pass
+
+
+def kepler_spline(time, flux, bkspace=1.5, maxiter=5, outlier_cut=3):
+    t_min, t_max = np.min(time), np.max(time)
+    time = (time - t_min) / (t_max - t_min)
+    bkspace /= t_max - t_min
+    spline = mask = None
+    for i in range(maxiter):
+        if spline is None: mask = np.ones_like(time, dtype=np.bool)
+        else:
+            new_mask = robust_mean(flux - spline, cut=outlier_cut)[-1]
+            if np.all(new_mask == mask): break
+            mask = new_mask
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                curve = bspline.iterfit(time[mask], flux[mask], bkspace=bkspace)[0]
+            spline = curve.value(time)[0]
+        except:
+            return
+    
+    return spline, mask
+
+
+def choose_kepler_spline(time, flux, bkspaces, maxiter=5, penalty_coeff=1.0, verbose=True):
+    abs_deviations = np.abs(flux[1:] - flux[:-1])
+    sigma = np.median(abs_deviations) * 1.48 / np.sqrt(2)
+    best_bic = best_spline = best_spline_mask = best_bkspace = None
+    bad_bkspaces = []
+    for bkspace in bkspaces:
+        nparams = npoints = ssr = 0
+        spline, spline_mask, bad_bkspace = [], [], False
+        for time, flux in zip(time, flux):
+            if len(time) < 4:
+                spline.append(flux)
+                spline_mask.append(np.ones_like(flux))
+                continue
+            response = kepler_spline(time, flux, bkspace=bkspace, maxiter=maxiter)
+            if response is None:
+                if verbose: warnings.warn("Bad bkspace")
+                bad_bkspaces.append(bkspace)
+                bad_bkspace = True
+                break
+
+            spline.append(spline_piece)
+            spline_mask.append(mask)
+            nparams += int((np.max(time) - np.min(time)) / bkspace) + 3
+            npoints += np,sum(mask)
+            ssr += np.sum((flux[mask] - spline_piece[mask]) ** 2)
+
+        if bad_bkspace: continue
+        bic = npoints * np.log(2 * np.pi * sigma ** 2) + ssr / sigma ** 2 + penalty_coeff * nparams * np.log(npoints)
+        if best_bic is None or bic < best_bic:
+            best_bic = bic
+            best_spline = spline
+            best_spline_mask = spline_mask
+            best_bkspace = bkspace
+
+    return best_spline, best_spline_mask, best_bkspace, bad_bkspaces
