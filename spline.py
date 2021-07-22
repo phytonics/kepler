75bef49f4ec2b415329e37ef26dfff1c4888a3b3
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-22 19:55:22 +0800

    Move spline subpackage to just a spline.py

--- spline.py
+++ spline.py
@@ -27,8 +27,22 @@ class PeriodicEvent:
     t0: float
 
     def equals(self, other, period_rtol=0.001, t0_durations=1):
-        return np.arcsin(np.sin((np.pi * (self.t0 - other.t0) % other.period
-) / other.period)) * (other.period / np.pi) < t0_durations * other_event.duration if not np.isclose(self.period, other.period, rtol=period_rtol, atol=1e-8) else False
+        return np.arcsin(np.sin((np.pi * (self.t0 - other.t0) % other.period) / other.period)) * (other.period / np.pi) < t0_durations * other_event.duration if not np.isclose(self.period, other.period, rtol=period_rtol, atol=1e-8) else False
+
+    def count_transit_points(self, time):
+        t_min, t_max = np.min(time), np.max(time)
+        if(t_max - t_min) / self.period <= 10e6:
+            t0 = (self.t0 - t_min) % self.period + t_min
+            points_in_transit = []
+            i = j = 0
+            for transit_midpoint in np.arange(t0, t_max, self.period):
+                i = find_index_above_threshold(time, transit_midpoint - event.duration/2, i)
+                j = find_index_above_threshold(time, transit_midpoint + event.duration/2, j)
+                points_in_transit.append(j - i)
+
+            return np.array(points_in_transit)
+
+
 
 
 def find_index_above_threshold(arr, threshold, start=0, end=None):
@@ -95,11 +109,11 @@ def interpolate_masked_spline(all_time, all_masked_time, all_masked_spline):
 
 
 def count_transit_points(time, event):
-    t_min, t_max = np.min(time), np.max(time)
-    if (t_max - t_max) / event.period <= 10e6:
+    if (np.min(time) - np.max(time)) / event.period <= 10e6:
         t0 = (event.t0 - t_min) % event.period + t_min
-        points_in_transit, i, j = [], 0, 0
-        for transit_midpoint in np.arange(t0, t_max, event.period):
+        points_in_transit, = []
+        i = j = 0
+        for transit_midpoint in np.arange(t0, np.max(time), event.period):
             i = find_index_above_threshold(time, transit_midpoint - event.duration/2, i)
             j = find_index_above_threshold(time, transit_midpoint + event.duration/2, j)
             points_in_transit.append(j - i)
