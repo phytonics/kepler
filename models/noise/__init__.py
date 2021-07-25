244b1716236a728188aa91cd83d147fe26c40cd5
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-07-26 00:15:00 +0800

    Add some stuff

--- models/noise/__init__.py
+++ models/noise/__init__.py
@@ -1,39 +1,30 @@
 from kepler.core.curve import LightCurve
-import lightkurve as lk
-from matplotlib import axes
-from typing import Union
+from kepler.core.base import LightCurveAction
 
-def removeNoise(lc: LightCurve):
-    """ Removes Instrumental noise from the light curve.
+def reduceNoise(lc: LightCurve):
+    """
+    Removes Instrumental noise from the light curve.
     
     This function uses the lightkurve library to perform this function
-    The lightkurve library uses Pixel Level Decorrelation(PLD)
+    The lightkurve library uses Pixel Level Decorrelation (PLD)
     A corrector for the curve is created and then applied to itself.
     The result of that is returned.
 
-    NOTE: This function will count exoplanet transits as noise
+    NOTE: This intention of this function is to count exoplanet transits as noise
 
-    Parameters
-    -----------------------------------------
-    lcID: 
-        ID of the light curve
-        It will be downloaded as a target pixel file
-        Ensure that this has a valid target pixel file
-    
-    quarters: int
-        The number of quarters to download
-        Reduce this number if function takes too long
-
-    Returns
-    -----------------------------------------
-    LightCurve
-        The light curve with reduced instrument noise
-
-    Diagnostics
-        Some values describing the extent of noise reduction
+    :param lc: The original light curve
+    :return The light curve with reduced instrumental noise, and some values describing the extent of the noise reduction.
     """
 
     pld = lc.to_corrector('pld')
     correct_lc = pld.correct()
 
+    # Some instructions:
+    # if you could, could you please research a bit more about noise reduction algorithms?
+
     return LightCurve(correct_lc, lc.id), pld.diagnose()
+
+
+class NoiseReduction(LightCurveAction):
+    def perform(self, lc):
+        return reduceNoise(lc)
